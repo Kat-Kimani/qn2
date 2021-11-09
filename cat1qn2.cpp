eb2b2192ec79207115c0e5ec2c226385aea7d4ce
Catherine Kimani <catherine.wachu@strathmore.edu>
2021-11-09 19:16:07 +0300

    Add project files.

---
+++ cat1qn2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	double mynum = 5.0 / 9.0;
+	float mynum2 = -32.0;
+	int mynum = int(mynum2);
+	cout << "mynewnum";
+
+
+
+
+
+	return 0;
+}
